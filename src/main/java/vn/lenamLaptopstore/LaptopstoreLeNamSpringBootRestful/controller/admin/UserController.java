596b2e4f58c8ec4c72fbeaab5d8649cfee181b2f
Le Thanh Nam <lethanhnam330@gmail.com>
2024-07-29 12:03:36 +0700

    add get user by page

--- src/main/java/vn/lenamLaptopstore/LaptopstoreLeNamSpringBootRestful/controller/admin/UserController.java
+++ src/main/java/vn/lenamLaptopstore/LaptopstoreLeNamSpringBootRestful/controller/admin/UserController.java
@@ -3,15 +3,23 @@ package vn.lenamLaptopstore.LaptopstoreLeNamSpringBootRestful.controller.admin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.turkraft.springfilter.boot.Filter;
+
 import jakarta.validation.Valid;
+import vn.lenamLaptopstore.LaptopstoreLeNamSpringBootRestful.domain.Product;
 import vn.lenamLaptopstore.LaptopstoreLeNamSpringBootRestful.domain.User;
 import vn.lenamLaptopstore.LaptopstoreLeNamSpringBootRestful.domain.Response.ResCreateUserDTO;
+import vn.lenamLaptopstore.LaptopstoreLeNamSpringBootRestful.domain.Response.ResultPaginationDTO;
 import vn.lenamLaptopstore.LaptopstoreLeNamSpringBootRestful.service.UserService;
 import vn.lenamLaptopstore.LaptopstoreLeNamSpringBootRestful.util.annotation.ApiMessage;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -25,6 +33,17 @@ public class UserController {
     public UserController(PasswordEncoder passwordEncoder, UserService userService) {
         this.passwordEncoder = passwordEncoder;
         this.userService = userService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<ResultPaginationDTO> handleGetUsers(@Filter Specification<User> specification,
+            Pageable pageable) {
+
+        Page<User> userPage = this.userService.getUsersByPageAndFilter(specification, pageable);
+
+        ResultPaginationDTO resultPaginationDTO = this.userService.convertToResultPaginationDTO(userPage);
+
+        return ResponseEntity.ok().body(resultPaginationDTO);
     }
 
     @PostMapping("")
--- src/main/java/vn/lenamLaptopstore/LaptopstoreLeNamSpringBootRestful/service/UserService.java
+++ src/main/java/vn/lenamLaptopstore/LaptopstoreLeNamSpringBootRestful/service/UserService.java
@@ -2,10 +2,15 @@ package vn.lenamLaptopstore.LaptopstoreLeNamSpringBootRestful.service;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import vn.lenamLaptopstore.LaptopstoreLeNamSpringBootRestful.domain.Product;
 import vn.lenamLaptopstore.LaptopstoreLeNamSpringBootRestful.domain.User;
 import vn.lenamLaptopstore.LaptopstoreLeNamSpringBootRestful.domain.Response.ResCreateUserDTO;
+import vn.lenamLaptopstore.LaptopstoreLeNamSpringBootRestful.domain.Response.ResultPaginationDTO;
 import vn.lenamLaptopstore.LaptopstoreLeNamSpringBootRestful.repository.UserRepository;
 
 @Service
@@ -23,6 +28,30 @@ public class UserService {
 
     public Optional<User> getUserByEmail(String email) {
         return this.userRepository.findFirstUserByEmail(email);
+    }
+
+    public Page<User> getUsersByPageAndFilter(Specification<User> specification,
+            Pageable pageable) {
+
+        Page<User> userPage = this.userRepository.findAll(specification, pageable);
+
+        return userPage;
+    }
+
+    public ResultPaginationDTO convertToResultPaginationDTO(Page<User> userPage) {
+
+        ResultPaginationDTO resultPaginationDTO = new ResultPaginationDTO();
+
+        ResultPaginationDTO.Meta meta = new ResultPaginationDTO.Meta();
+        meta.setPage(userPage.getNumber() + 1);
+        meta.setPageSize(userPage.getSize());
+        meta.setPages(userPage.getTotalPages());
+        meta.setTotal(userPage.getTotalElements());
+
+        resultPaginationDTO.setResult(userPage.getContent());
+        resultPaginationDTO.setMeta(meta);
+
+        return resultPaginationDTO;
     }
 
     public ResCreateUserDTO handleConvertUserToResCreateUserDTO(User user) {

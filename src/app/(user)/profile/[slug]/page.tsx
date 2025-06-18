af17a795473e7af50a807c85e4edd2ff9338a8f1
Le Thanh Nam <lethanhnam330@gmail.com>
2025-06-18 09:20:53 +0700

    update

--- src/app/(user)/profile/[slug]/page.tsx
+++ src/app/(user)/profile/[slug]/page.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import ProfileUser from '@/components/Profile/profile.user';
 import Box from '@mui/material/Box';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import {
-    handleGetUserRandomAction,
-    handleGetOneUserById,
-    handleGetPostsByUserId,
-    handleGetUserByFollowing,
-} from '@/utils/actions/actions';
 
 export default async function ProfilePage({ params }: { params: { slug: string } }) {
     const { slug } = params;
